e6f7fbab180e250414a2f77a1de0540e17da02c8
honorsuper <honorsuper@126.com>
2022-12-01 22:14:03 +0800

    feat: 插入图片资源

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import './index.less';
+import reactImg from '@/assets/react.svg';
 
 function App() {
-  return <div className="App">my vite config</div>;
+  return (
+    <div className="app">
+      <div className="title">my vite project</div>
+      <img src={reactImg} />
+    </div>
+  );
 }
 
 export default App;
