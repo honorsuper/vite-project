9f4e624b4d28b6796bbedcb45e42667e620feae4
honorsuper <honorsuper@126.com>
2022-12-01 13:46:17 +0800

    feat: 配置less和主题色

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, normalizePath } from "vite";
+import react from "@vitejs/plugin-react";
+import autoprefixer from "autoprefixer";
+import path from "path";
+
+// 全局 scss 文件的路径
+// 用 normalizePath 解决 window 下的路径问题
+const variablePath = normalizePath(path.resolve("./src/variable.less"));
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  css: {
+    // 进行 PostCSS 配置
+    postcss: {
+      plugins: [
+        autoprefixer({
+          // 指定目标浏览器
+          overrideBrowserslist: ["Chrome > 40", "ff > 31", "ie 11"],
+        }),
+      ],
+    },
+    preprocessorOptions: {
+      less: {
+        // additionalData 的内容会在每个 less 文件的开头自动注入
+        additionalData: `@import "${variablePath}";`,
+      },
+    },
+  },
+});
